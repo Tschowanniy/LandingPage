3bff2ddc4d19634187399e0d193afdbf0c3ff14f
Tschowanniy <62508500+Tschowanniy@users.noreply.github.com>
2022-08-10 08:36:14 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -87,7 +87,7 @@ function scroller() {
     let element = document.getElementById(secId);
     //console.log(element);
     // and use finally the scrollintoview function to scroll to the section.
-    element.scrollIntoView(true); 
+    element.scrollIntoView(true {behavior: "smooth"}); 
 }
 
 // clicknavbar recognises a click onto the navigation menu
